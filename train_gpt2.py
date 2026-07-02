7b4676761c46fd0de19494f3b9549bbf6fc27d6f
Tomo Suzuki <suztomo@gmail.com>
2026-07-02 14:32:13 +0000

    Add Japanese Wikipedia tokenization script and update train_gpt2.py data_root

--- train_gpt2.py
+++ train_gpt2.py
@@ -220,7 +220,7 @@ class DataLoaderLite:
         assert split in {'train', 'val'}
 
         # get the shard filenames
-        data_root = "edu_fineweb10B"
+        data_root = "wiki_ja"
         shards = os.listdir(data_root)
         shards = [s for s in shards if split in s]
         shards = sorted(shards)
